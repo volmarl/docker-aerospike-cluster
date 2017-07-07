ff77c1677681f31461015b1bb7e4db975bbc2fac
Lucien Volmar <lucien@aerospike.com>
2017-07-06 18:18:10 -0700

    Aerospike Cluster in Docker

---
+++ README.md
@@ -0,0 +1,16 @@
+# Usage
+
+```bash
+docker stack deploy -c aerospike.yml aerospike  
+```
+
+1. Verify your deployment:
+    `docker stack ls`
+1. Verify your services:
+    `docker service ls`
+1. Take a look at your containers:
+    `docker ps`
+1. To remove the docker swarm stack:
+`docker stack rm aerospike`
+
+
---
+++ addtools.sh
@@ -0,0 +1,12 @@
+apt-get update -y
+apt-get install vim -y
+apt-get install dnsutils -y
+
+{
+echo "start background discovery in 10 secs"
+sleep 10 
+for i in `host ${AEROSPIKE_ENV}|grep 'has address'|awk '{print $4}'`; do asinfo -v "tip:host=${i};port=3002"; done
+echo "Finished Cluster Discovery"
+}&
+echo "starting asd"
+asd --config-file /run/secrets/aerospike.conf --foreground 
